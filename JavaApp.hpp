a9540e78ccc5536d910528577d333ee6c6aaa010
Michael L. Collard <collard@uakron.edu>
2021-11-03 17:06:16 -0400

    Add start for Visitor

---
+++ JavaApp.hpp
@@ -0,0 +1,18 @@
+/*
+    JavaApp.hpp
+
+    Application built on JavaVisitor.
+*/
+
+#ifndef INCLUDED_JAVAAPP_HPP
+#define INCLUDED_JAVAAPP_HPP
+
+#include <iostream>
+#include "JavaVisitorBlock.h"
+
+class  JavaApp : public JavaVisitor {
+public:
+
+};
+
+#endif
